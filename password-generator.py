1a6d0cd856b67afe626d436909a5394653cc08ee
Drishya Agarwal <106886727+Drishyaz@users.noreply.github.com>
2022-06-30 22:52:59 +0530

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -1,3 +1,13 @@
+## password-generator
+
+#The program takes user input from the user that is, how much length of password does the user want?
+#Enter password length: 
+#8
+
+#Generates a random password of length 8. E.g,
+#Your password is: 
+#Ue=$tP&r
+
 import string
 import random
 
